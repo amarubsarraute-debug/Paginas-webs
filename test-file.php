ef21ad7142447253c44d95d9d447efa63bde63fc
Amaru Belline <amarubsarraute@gmail.com>
2026-07-07 17:42:59 -0500

    Actualizar script de diagnostico para listar subdirectorios

--- test-file.php
+++ test-file.php
@@ -1,31 +1,27 @@
 <?php
 header('Content-Type: text/plain');
 
-$projects = [
-    'silvera-electricidad',
-    'electricista-milton-sardella',
-    'electricidad-ocean-park-electricista-facundo-azcurra',
-    'noguera-electricista-autorizado-por-ute',
-    'barcelo-instalaciones-electricas',
-    'prolighting',
-    'juan-carlos-martinez-electricidad',
-    'alejandro-severo-electricista-autorizado-x-ute'
-];
+echo "Current Working Directory: " . getcwd() . "\n\n";
 
-foreach ($projects as $proj) {
-    echo "=== PROJECT: $proj ===\n";
-    $imgDir = "$proj/img";
-    if (file_exists($imgDir) && is_dir($imgDir)) {
-        $files = scandir($imgDir);
-        foreach ($files as $file) {
-            if ($file !== '.' && $file !== '..') {
-                $filePath = "$imgDir/$file";
-                echo "  - $file (" . filesize($filePath) . " bytes)\n";
+$dir = 'silvera-electricidad';
+if (file_exists($dir) && is_dir($dir)) {
+    echo "Contents of $dir:\n";
+    $files = scandir($dir);
+    foreach ($files as $file) {
+        if ($file !== '.' && $file !== '..') {
+            $path = "$dir/$file";
+            echo "  - $file (" . (is_dir($path) ? 'Dir' : 'File') . ")\n";
+            if (is_dir($path)) {
+                $subFiles = scandir($path);
+                foreach ($subFiles as $sub) {
+                    if ($sub !== '.' && $sub !== '..') {
+                        echo "    |- $sub\n";
+                    }
+                }
             }
         }
-    } else {
-        echo "  [ERROR] img/ directory does not exist or is not a directory.\n";
     }
-    echo "\n";
+} else {
+    echo "Directory $dir does not exist.\n";
 }
 ?>
